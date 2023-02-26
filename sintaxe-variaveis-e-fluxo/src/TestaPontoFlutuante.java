3256e1d4fdae1fd9cb520ec62d40d4e2b3df6361
RivaniaGomes <rivaniagomes@gmail.com>
2023-02-26 20:22:14 -0300

    tipos e variáveis

---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,20 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		double salario;
+		salario = 1350.70;
+		System.out.println("meu salario e " + salario);
+		
+		double divisao = 3.14 / 2;
+		System.out.println(divisao);
+		
+		int outraDivisaO = 5/2;
+		System.out.println(outraDivisaO);
+		
+		double outraTentativa = 5.0 /2;
+		System.out.println(outraTentativa);
+
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,21 @@
+
+/**
+ * @author rivania.dsn.pack
+ *
+ */
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("Olá, novo teste");
+
+		int idade;
+		idade = 37;
+		System.out.println("sua idade é " + idade);
+
+		idade = 30 + 10;
+		System.out.println("sua idade é " + idade);
+
+		idade = (7 * 5) + 2;
+		System.out.println("sua idade é " + idade);
+		
+	}
+}
